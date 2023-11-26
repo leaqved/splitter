3fa61d461cfd91be08aac28db6c02091bfba0934
leaqved <leaqved+dev@gmail.com>
2023-11-26 10:16:06 +0300

    feat(splitter): add "Process" method

--- splitter.go
+++ splitter.go
@@ -21,3 +21,26 @@ func (s *Splitter) Check(rules ...Rule) bool {
 	}
 	return false
 }
+
+func (s *Splitter) Process(str string) []string {
+	s.Buffer.Incoming = []rune(str)
+	for !s.Buffer.IsDone() {
+		switch {
+		case s.Check(s.Skip...):
+			s.Buffer.Trim()
+		case s.Check(s.Split...):
+			s.Buffer.Store()
+			fallthrough
+		default:
+			if s.Check(s.Join...) {
+				s.Buffer.Load()
+			} else {
+				s.Buffer.Trim()
+			}
+		}
+	}
+	if !s.Buffer.IsEmpty() {
+		s.Buffer.Store()
+	}
+	return s.Buffer.Done
+}
