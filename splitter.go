575833ea759f2763371b20022dcd0841d9bb581b
leaqved <leaqved+dev@gmail.com>
2023-11-26 10:10:49 +0300

    feat(splitter): add "Check" method

--- splitter.go
+++ splitter.go
@@ -12,3 +12,12 @@ func New() *Splitter {
 		Buffer: &Buffer{},
 	}
 }
+
+func (s *Splitter) Check(rules ...Rule) bool {
+	for _, rule := range rules {
+		if rule(s.Buffer) {
+			return true
+		}
+	}
+	return false
+}
