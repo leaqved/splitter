2a00be7c6d9bfe9aa0fc078844b430785e2966f4
leaqved <leaqved+dev@gmail.com>
2023-11-27 05:57:30 +0300

    docs(rule): add descriptions

--- rule.go
+++ rule.go
@@ -1,3 +1,9 @@
+// Package splitter implements a string splitting package.
+// It defines a type, [Splitter], with [Buffer] to store and change data,
+// and [Rule] functions that control its behavior.
 package splitter
 
+// Rule function gains access to [Buffer] data using its public methods.
+//
+// Should return true if current [Buffer] state satisfies the rule and false otherwise.
 type Rule func(*Buffer) bool
