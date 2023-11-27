a8321d444b03a34a10f6444851a00ff44d2b1eae
leaqved <leaqved+dev@gmail.com>
2023-11-27 05:50:57 +0300

    docs(splitter): add descriptions

--- splitter.go
+++ splitter.go
@@ -1,5 +1,10 @@
+// Package splitter implements a string splitting package.
+// It defines a type, [Splitter], with [Buffer] to store and change data,
+// and [Rule] functions that control its behavior.
 package splitter
 
+// A Splitter represents an active object that
+// splits strings according to [Rule] functions.
 type Splitter struct {
 	buffer *Buffer
 	skip   []Rule
@@ -7,6 +12,7 @@ type Splitter struct {
 	join   []Rule
 }
 
+// New creates a new [Splitter] and applies [Option] functions to it.
 func New(opts ...Option) *Splitter {
 	s := &Splitter{
 		buffer: &Buffer{},
@@ -17,6 +23,9 @@ func New(opts ...Option) *Splitter {
 	return s
 }
 
+// check applies [Rule] functions to the [Splitter]'s buffer.
+//
+// Returns true if any of [Rule] functions returns true and false otherwise.
 func (s *Splitter) check(rules ...Rule) bool {
 	for _, rule := range rules {
 		if rule(s.buffer) {
@@ -26,6 +35,9 @@ func (s *Splitter) check(rules ...Rule) bool {
 	return false
 }
 
+// Split splits the string according to [Rule] functions of the [Splitter].
+//
+// Returns the resulting slice.
 func (s *Splitter) Split(str string) []string {
 	s.buffer.incoming = []rune(str)
 	for !s.buffer.isDone() {
