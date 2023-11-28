5ad5d3e5fb3a83930dc722118fe2e77a65136604
leaqved <leaqved+dev@gmail.com>
2023-11-28 09:49:10 +0300

    docs(rule): add descriptions

--- rule.go
+++ rule.go
@@ -8,8 +8,10 @@ package splitter
 // Should return true if current [Buffer] state satisfies the rule and false otherwise.
 type Rule func(*Buffer) bool
 
+// CharRule function should return true if given character satisfies the rule and false otherwise.
 type CharRule func(char rune) bool
 
+// Before returns a Rule that checks if the next character incoming in [Buffer] satisfies the CharRule.
 func Before(rule CharRule) Rule {
 	return func(b *Buffer) bool {
 		char, ok := b.GetRight()
@@ -20,6 +22,7 @@ func Before(rule CharRule) Rule {
 	}
 }
 
+// After returns a Rule that checks if the last character in [Buffer] satisfies the CharRule.
 func After(rule CharRule) Rule {
 	return func(b *Buffer) bool {
 		char, ok := b.GetLeft()
@@ -30,12 +33,14 @@ func After(rule CharRule) Rule {
 	}
 }
 
+// And returns a logical conjunction of two Rules.
 func (r Rule) And(another Rule) Rule {
 	return func(b *Buffer) bool {
 		return r(b) && another(b)
 	}
 }
 
+// And returns a logical conjunction of two CharRules.
 func (r CharRule) And(another CharRule) CharRule {
 	return func(char rune) bool {
 		return r(char) && another(char)
